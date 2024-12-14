b9ecf121c6663405b2ac885f26b33d2f3392ef0f
Niall Melvin <niallmelvin@rcsi.com>
2024-12-14 13:43:21 +0000

    Deploy to heroku

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def my_blog(request):
+    # return render(request, 'home.html') # Ensure 'home.html' exists in your templates directory
+    return HttpResponse("Hello, Blog!")
