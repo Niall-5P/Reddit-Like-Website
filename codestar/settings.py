fcb2ab3fa35056daa6ef89d607c7f3595006e449
Niall Melvin <niallmelvin@rcsi.com>
2025-01-02 23:38:33 +0000

    Fix anchor tag in html

--- codestar/settings.py
+++ codestar/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 
 DATABASES = {
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
